6f952aaa84290070034c57dac679b5c9973c59e5
andrew <andrewmoren93@gmail.com>
2022-06-01 19:59:31 +0300

    add forms + php

--- js/script.js
+++ js/script.js
@@ -199,4 +199,46 @@ document.addEventListener("DOMContentLoaded", () => {
     21,
     ".menu .container"
   ).render();
+
+  // Forms
+
+  const forms = document.querySelectorAll("form");
+
+  const message = {
+    loading: "Загрузка",
+    success: "Спасибо, скоры мы с вами свяжемся",
+    failure: "Что-то пошло не так...",
+  };
+
+  forms.forEach((item) => {
+    postData(item);
+  });
+
+  function postData(form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const statusMessage = document.createElement("div");
+      statusMessage.classList.add("status");
+      statusMessage.textContent = message.loading;
+      form.append(statusMessage);
+
+      const request = new XMLHttpRequest();
+      request.open("POST", "server.php");
+
+      request.setRequestHeader("Content-type", "multipart/form-data");
+      const formData = new FormData(form);
+
+      request.send(formData);
+
+      request.addEventListener("load", () => {
+        if (request.status === 200) {
+          console.log(request.response);
+          statusMessage.textContent = message.success;
+        } else {
+          statusMessage.textContent = message.failure;
+        }
+      });
+    });
+  }
 });
