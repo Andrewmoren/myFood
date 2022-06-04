07dbd21e61aa521a2bb09a31d0be7490222f6902
andrew <andrewmoren93@gmail.com>
2022-06-04 19:59:08 +0300

    add  fetch API

--- js/script.js
+++ js/script.js
@@ -223,32 +223,32 @@ document.addEventListener("DOMContentLoaded", () => {
           `;
       form.insertAdjacentElement("afterend", statusMessage);
 
-      const request = new XMLHttpRequest();
-      request.open("POST", "server.php");
-      request.setRequestHeader(
-        "Content-type",
-        "application/json; charset=utf-8"
-      );
       const formData = new FormData(form);
 
       const object = {};
       formData.forEach(function (value, key) {
         object[key] = value;
       });
-      const json = JSON.stringify(object);
 
-      request.send(json);
-
-      request.addEventListener("load", () => {
-        if (request.status === 200) {
-          console.log(request.response);
+      fetch("server.php", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json; charset=utf-8",
+        },
+        body: JSON.stringify(object),
+      })
+        .then((data) => data.text())
+        .then((data) => {
+          console.log(data);
           showThanksModal(message.success);
           statusMessage.remove();
-          form.reset();
-        } else {
+        })
+        .catch(() => {
           showThanksModal(message.failure);
-        }
-      });
+        })
+        .finally(() => {
+          form.reset();
+        });
     });
   }
   function showThanksModal(message) {
@@ -273,14 +273,4 @@ document.addEventListener("DOMContentLoaded", () => {
       closeModal();
     }, 4000);
   }
-
-  fetch("https://jsonplaceholder.typicode.com/posts", {
-    method: "POST",
-    body: JSON.stringify({ name: "Alex" }),
-    headers: {
-      "Content-type": "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
 });
