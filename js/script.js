781c720f6b6f249b47f002a1019ccfa2aad97746
andrew <andrewmoren93@gmail.com>
2022-06-04 19:30:41 +0300

    add fake fetch API JSONPlaceholder

--- js/script.js
+++ js/script.js
@@ -273,4 +273,14 @@ document.addEventListener("DOMContentLoaded", () => {
       closeModal();
     }, 4000);
   }
+
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({ name: "Alex" }),
+    headers: {
+      "Content-type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
 });
