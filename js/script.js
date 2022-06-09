58d40b6403c010f50298558ef61922b140f4e196
andrew <andrewmoren93@gmail.com>
2022-06-09 20:15:17 +0300

    add js-server

--- js/script.js
+++ js/script.js
@@ -273,4 +273,8 @@ document.addEventListener("DOMContentLoaded", () => {
       closeModal();
     }, 4000);
   }
+
+  fetch("http://localhost:3000/menu")
+    .then((data) => data.json())
+    .then((res) => console.log(res));
 });
