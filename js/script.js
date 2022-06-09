15f1af55c28aa686f6eb1ec094ded7fd6243a0c1
andrew <andrewmoren93@gmail.com>
2022-06-09 20:36:17 +0300

    add async/away

--- js/script.js
+++ js/script.js
@@ -208,10 +208,22 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
   forms.forEach((item) => {
-    postData(item);
+    bindPostData(item);
   });
 
-  function postData(form) {
+  const postData = async (url, data) => {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json; charset=utf-8",
+      },
+      body: data,
+    });
+
+    return await res.json();
+  };
+
+  function bindPostData(form) {
     form.addEventListener("submit", (e) => {
       e.preventDefault();
 
@@ -230,14 +242,7 @@ document.addEventListener("DOMContentLoaded", () => {
         object[key] = value;
       });
 
-      fetch("server.php", {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json; charset=utf-8",
-        },
-        body: JSON.stringify(object),
-      })
-        .then((data) => data.text())
+      postData("http://localhost:3000/requests", JSON.stringify(object))
         .then((data) => {
           console.log(data);
           showThanksModal(message.success);
