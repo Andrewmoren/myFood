763a53325791707ff4bd1403dd6774f194882c78
andrew <andrewmoren93@gmail.com>
2022-06-14 18:11:36 +0300

    add axios library

--- js/script.js
+++ js/script.js
@@ -181,8 +181,21 @@ document.addEventListener("DOMContentLoaded", () => {
     return await res.json();
   };
 
-  getResource("http://localhost:3000/menu").then((data) => {
-    data.forEach(({ img, altimg, title, descr, price }) => {
+  // getResource("http://localhost:3000/menu").then((data) => {
+  // data.forEach(({ img, altimg, title, descr, price }) => {
+  //   new MenuCard(
+  //     img,
+  //     altimg,
+  //     title,
+  //     descr,
+  //     price,
+  //     ".menu .container"
+  //   ).render();
+  //   });
+  // });
+
+  axios.get("http://localhost:3000/menu").then((data) => {
+    data.data.forEach(({ img, altimg, title, descr, price }) => {
       new MenuCard(
         img,
         altimg,
