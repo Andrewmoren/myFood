002854d623ac0dc4b38480f96f34edf328e6a010
andrew <andrewmoren93@gmail.com>
2022-06-15 23:12:28 +0300

    addEvent next

--- js/script.js
+++ js/script.js
@@ -301,12 +301,27 @@ document.addEventListener("DOMContentLoaded", () => {
   const width = window.getComputedStyle(slidesWrapper).width;
 
   let slideIndex = 1;
+  let offset = 0;
 
   slidesField.style.width = 100 * slides.length + "%";
+  slidesField.style.display = "flex";
+  slidesField.style.transition = "0.5s all";
+
+  slidesWrapper.style.overflow = "hidden";
+
   slides.forEach((slide) => {
     slide.style.width = width;
   });
 
+  next.addEventListener("click", () => {
+    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+      offset = 0;
+    } else {
+      offset += +width.slice(0, width.length - 2);
+    }
+
+    slidesField.style.transform = `translateX(-${offset}px)`;
+  });
   // showSlides(slideIndex);
 
   // if (slides.length < 10) {
