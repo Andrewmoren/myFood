fb28687b76a47e1ef34f516d5bc60974ef23b95a
andrew <andrewmoren93@gmail.com>
2022-06-20 20:57:16 +0300

    add function dynamic

--- js/script.js
+++ js/script.js
@@ -481,38 +481,40 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function calcTotal() {
     if (!sex || !height || !weight || !age || !ratio) {
-      result.textContent = "Укажите все данные!";
+      result.textContent = "____";
       return;
     }
 
     if (sex === "female") {
-      result.textContent =
-        (447.6 + 9.2 * weight + 3.1 * height - 4.3 * age) * ratio;
+      result.textContent = Math.round(
+        (447.6 + 9.2 * weight + 3.1 * height - 4.3 * age) * ratio
+      );
     } else {
-      result.textContent =
-        88.36 + 13.4 * weight + 4.8 * height - 5.7 * age * ratio;
+      result.textContent = Math.round(
+        (88.36 + 13.4 * weight + 4.8 * height - 5.7 * age) * ratio
+      );
     }
   }
 
   calcTotal();
 
-  function getStaticInformation(parenSelector, activClass) {
-    const elements = document.querySelectorAll(`${parenSelector} div`);
+  function getStaticInformation(parentSelector, activeClass) {
+    const elements = document.querySelectorAll(`${parentSelector} div`);
 
-    document.querySelector(parenSelector).addEventListener("click", (e) => {
+    document.querySelector(parentSelector).addEventListener("click", (e) => {
       if (e.target.getAttribute("data-ratio")) {
         ratio = +e.target.getAttribute("data-ratio");
       } else {
         sex = e.target.getAttribute("id");
       }
 
-      console.log(ratio, sex);
-
       elements.forEach((elem) => {
-        elem.classList.remove(activClass);
+        elem.classList.remove(activeClass);
       });
 
-      e.target.classList.add(activClass);
+      e.target.classList.add(activeClass);
+
+      calcTotal();
     });
   }
 
@@ -521,4 +523,28 @@ document.addEventListener("DOMContentLoaded", () => {
     ".calculating__choose_big",
     "calculating__choose-item_active"
   );
+
+  function getDynamicInformation(selector) {
+    const input = document.querySelector(selector);
+
+    input.addEventListener("input", () => {
+      switch (input.getAttribute("id")) {
+        case "height":
+          height = +input.value;
+          break;
+        case "weight":
+          weight = +input.value;
+          break;
+        case "age":
+          age = +input.value;
+          break;
+      }
+
+      calcTotal();
+    });
+  }
+
+  getDynamicInformation("#height");
+  getDynamicInformation("#weight");
+  getDynamicInformation("#age");
 });
