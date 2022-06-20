88d245efe1c2d35c0a99bd9152750995c317c650
andrew <andrewmoren93@gmail.com>
2022-06-20 21:05:03 +0300

    working  calc

--- js/script.js
+++ js/script.js
@@ -477,7 +477,11 @@ document.addEventListener("DOMContentLoaded", () => {
   // Calculator
 
   const result = document.querySelector(".calculating__result span");
-  let sex, height, weight, age, ratio;
+  let sex = "female",
+    height,
+    weight,
+    age,
+    ratio = 1.375;
 
   function calcTotal() {
     if (!sex || !height || !weight || !age || !ratio) {
@@ -501,20 +505,22 @@ document.addEventListener("DOMContentLoaded", () => {
   function getStaticInformation(parentSelector, activeClass) {
     const elements = document.querySelectorAll(`${parentSelector} div`);
 
-    document.querySelector(parentSelector).addEventListener("click", (e) => {
-      if (e.target.getAttribute("data-ratio")) {
-        ratio = +e.target.getAttribute("data-ratio");
-      } else {
-        sex = e.target.getAttribute("id");
-      }
+    elements.forEach((elem) => {
+      elem.addEventListener("click", (e) => {
+        if (e.target.getAttribute("data-ratio")) {
+          ratio = +e.target.getAttribute("data-ratio");
+        } else {
+          sex = e.target.getAttribute("id");
+        }
 
-      elements.forEach((elem) => {
-        elem.classList.remove(activeClass);
+        elements.forEach((elem) => {
+          elem.classList.remove(activeClass);
+        });
+
+        e.target.classList.add(activeClass);
+
+        calcTotal();
       });
-
-      e.target.classList.add(activeClass);
-
-      calcTotal();
     });
   }
 
