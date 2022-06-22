c2399b51310087aa55a768c66982a4ee9a5c2ce0
andrew <andrewmoren93@gmail.com>
2022-06-22 21:37:15 +0300

    delete slideIndex

--- js/script.js
+++ js/script.js
@@ -436,43 +436,6 @@ document.addEventListener("DOMContentLoaded", () => {
       dots[slideIndex - 1].style.opacity = 1;
     });
   });
-  // showSlides(slideIndex);
-
-  // if (slides.length < 10) {
-  //   total.textContent = `0${slides.length}`;
-  // } else {
-  //   total.textContent = slides.length;
-  // }
-
-  // function showSlides(n) {
-  //   if (n > slides.length) {
-  //     slideIndex = 1;
-  //   }
-  //   if (n < 1) {
-  //     slideIndex = slides.length;
-  //   }
-
-  //   slides.forEach((item) => (item.style.display = "none"));
-
-  //   slides[slideIndex - 1].style.display = "block";
-
-  //   if (slides.length < 10) {
-  //     current.textContent = `0${slideIndex}`;
-  //   } else {
-  //     current.textContent = slideIndex;
-  //   }
-  // }
-
-  // function plusSlides(n) {
-  //   showSlides((slideIndex += n));
-  // }
-
-  // prev.addEventListener("click", () => {
-  //   plusSlides(-1);
-  // });
-  // next.addEventListener("click", () => {
-  //   plusSlides(1);
-  // });
 
   // Calculator
 
