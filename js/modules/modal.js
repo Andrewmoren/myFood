9973cfc5d04e742de81cf567bbeae82db704a929
andrew <andrewmoren93@gmail.com>
2022-06-30 21:29:46 +0300

    add webpack modules 3

---
+++ js/modules/modal.js
@@ -0,0 +1,51 @@
+function modalWindow() {
+  // Modal window
+
+  const modalTrigger = document.querySelectorAll("[data-modal]");
+  const modal = document.querySelector(".modal");
+
+  function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    document.body.style.overflow = "hidden";
+    clearInterval(modalTimerID);
+  }
+
+  modalTrigger.forEach((btn) => {
+    btn.addEventListener("click", openModal);
+  });
+
+  function closeModal() {
+    modal.classList.remove("show");
+    modal.classList.add("hide");
+    document.body.style.overflow = "";
+  }
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal || e.target.getAttribute("data-close") == "") {
+      closeModal();
+    }
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape" && modal.classList.contains("show")) {
+      closeModal();
+    }
+  });
+
+  const modalTimerID = setTimeout(openModal, 50000);
+
+  function showModalByScroll() {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight - 1
+    ) {
+      openModal();
+      window.removeEventListener("scroll", showModalByScroll);
+    }
+  }
+
+  window.addEventListener("scroll", showModalByScroll);
+}
+
+module.exports = modalWindow;
--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const tabs = require("./modules/tabs");
+  const modal = require("./modules/modal");
+  const timer = require("./modules/timer");
+  const cards = require("./modules/cards");
+  const calculator = require("./modules/calculator");
+  const forms = require("./modules/forms");
+  const slider = require("./modules/slider");
+
+  tabs();
+  modal();
+  timer();
+  cards();
+  calculator();
+  forms();
+  slider();
+});
